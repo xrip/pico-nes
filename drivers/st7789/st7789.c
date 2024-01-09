e52460e3a19fb738b61a08ba68cf445ac149e925
xrip <xrip@xrip.ru>
2024-01-09 13:51:41 +0300

    optimizations

--- drivers/st7789/st7789.c
+++ drivers/st7789/st7789.c
@@ -60,13 +60,13 @@ static const uint8_t init_seq[] = {
 // Format: cmd length (including cmd byte), post delay in units of 5 ms, then cmd payload
 // Note the delays have been shortened a little
 
-static inline void lcd_set_dc_cs(bool dc, bool cs) {
+static inline void lcd_set_dc_cs(const bool dc, const bool cs) {
     sleep_us(1);
     gpio_put_masked((1u << PIN_DC) | (1u << PIN_CS), !!dc << PIN_DC | !!cs << PIN_CS);
     sleep_us(1);
 }
 
-static inline void lcd_write_cmd(PIO pio, uint sm, const uint8_t* cmd, size_t count) {
+static inline void lcd_write_cmd(const uint8_t* cmd, size_t count) {
     st7789_lcd_wait_idle(pio, sm);
     lcd_set_dc_cs(0, 0);
     st7789_lcd_put(pio, sm, *cmd++);
@@ -80,11 +80,10 @@ static inline void lcd_write_cmd(PIO pio, uint sm, const uint8_t* cmd, size_t co
     lcd_set_dc_cs(1, 1);
 }
 
-static inline void lcd_set_window(PIO pio, uint sm,
-                                  uint16_t x,
-                                  uint16_t y,
-                                  uint16_t width,
-                                  uint16_t height) {
+static inline void lcd_set_window(const uint16_t x,
+                                  const uint16_t y,
+                                  const uint16_t width,
+                                  const uint16_t height) {
     static uint8_t screen_width_cmd[] = { 0x2a, 0x00, 0x00, SCREEN_WIDTH >> 8, SCREEN_WIDTH & 0xff };
     static uint8_t screen_height_command[] = { 0x2b, 0x00, 0x00, SCREEN_HEIGHT >> 8, SCREEN_HEIGHT & 0xff };
     screen_width_cmd[2] = x;
@@ -92,27 +91,28 @@ static inline void lcd_set_window(PIO pio, uint sm,
 
     screen_height_command[2] = y;
     screen_height_command[4] = y + height - 1;
-    lcd_write_cmd(pio, sm, screen_width_cmd, 5);
-    lcd_write_cmd(pio, sm, screen_height_command, 5);
+    lcd_write_cmd(screen_width_cmd, 5);
+    lcd_write_cmd(screen_height_command, 5);
 }
 
-static inline void lcd_init(PIO pio, uint sm, const uint8_t* init_seq) {
+static inline void lcd_init(const uint8_t* init_seq) {
     const uint8_t* cmd = init_seq;
     while (*cmd) {
-        lcd_write_cmd(pio, sm, cmd + 2, *cmd);
+        lcd_write_cmd(cmd + 2, *cmd);
         sleep_ms(*(cmd + 1) * 5);
         cmd += *cmd + 2;
     }
 }
 
-static inline void start_pixels(PIO pio, uint sm) {
-    uint8_t cmd = 0x2c; // RAMWR
-    lcd_write_cmd(pio, sm, &cmd, 1);
+static inline void start_pixels() {
+    const uint8_t cmd = 0x2c; // RAMWR
+    lcd_write_cmd(&cmd, 1);
     lcd_set_dc_cs(1, 0);
 }
 
 void graphics_init() {
-    uint offset = pio_add_program(pio, &st7789_lcd_program);
+    const uint offset = pio_add_program(pio, &st7789_lcd_program);
+    sm = pio_claim_unused_sm(pio, true);
     st7789_lcd_program_init(pio, sm, offset, PIN_DIN, PIN_CLK, SERIAL_CLK_DIV);
 
     gpio_init(PIN_CS);
@@ -126,12 +126,12 @@ void graphics_init() {
 
     gpio_put(PIN_CS, 1);
     gpio_put(PIN_RESET, 1);
-    lcd_init(pio, sm, init_seq);
+    lcd_init(init_seq);
     gpio_put(PIN_BL, 1);
     clrScr(0);
 }
 
-enum graphics_mode_t graphics_set_mode(enum graphics_mode_t mode) {
+enum graphics_mode_t graphics_set_mode(const enum graphics_mode_t mode) {
     if (mode == graphics_mode) return graphics_mode;
 
     graphics_mode = mode;
@@ -143,7 +143,7 @@ enum graphics_mode_t graphics_set_mode(enum graphics_mode_t mode) {
     return graphics_mode;
 }
 
-void graphics_set_buffer(uint8_t* buffer, uint16_t width, uint16_t height) {
+void graphics_set_buffer(uint8_t* buffer, const uint16_t width, const uint16_t height) {
     graphics_buffer = buffer;
     graphics_buffer_width = width;
     graphics_buffer_height = height;
@@ -153,28 +153,26 @@ void graphics_set_textbuffer(uint8_t* buffer) {
     text_buffer = buffer;
 }
 
-void graphics_set_offset(int x, int y) {
+void graphics_set_offset(const int x, const int y) {
     graphics_buffer_shift_x = x;
     graphics_buffer_shift_y = y;
 }
 
-void clrScr(uint8_t color) {
-    lcd_set_window(pio, sm, 0, 0,SCREEN_WIDTH,SCREEN_HEIGHT);
-    start_pixels(pio, sm);
+void clrScr(const uint8_t color) {
+    lcd_set_window(0, 0,SCREEN_WIDTH,SCREEN_HEIGHT);
+    start_pixels();
     uint32_t i = SCREEN_WIDTH * SCREEN_HEIGHT;
-    do {
-        st7789_lcd_put(pio, sm, color);
-        st7789_lcd_put(pio, sm, color);
+    while (i--) {
+        st7789_lcd_put16(pio, sm, color);
     }
-    while (--i);
 }
 
 void draw_text(char* string, uint32_t x, uint32_t y, uint8_t color, uint8_t bgcolor) {
     uint8_t* t_buf = text_buffer + TEXTMODE_COLS * 2 * y + 2 * x;
     for (int xi = TEXTMODE_COLS * 2; xi--;) {
-        if (!(*string)) break;
+        if (!*string) break;
         *t_buf++ = *string++;
-        *t_buf++ = (bgcolor << 4) | (color & 0xF);
+        *t_buf++ = bgcolor << 4 | color & 0xF;
     }
 }
 
@@ -204,43 +202,40 @@ void draw_window(char* title, uint32_t x, uint32_t y, uint32_t width, uint32_t h
 void __scratch_y("refresh_lcd") refresh_lcd() {
     switch (graphics_mode) {
         case TEXTMODE_80x30:
-            lcd_set_window(pio, sm, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+            lcd_set_window(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
             start_pixels(pio, sm);
 
             for (int y = 0; y < SCREEN_HEIGHT; y++) {
-                st7789_lcd_put(pio, sm, 0x00);
-                st7789_lcd_put(pio, sm, 0x00);
+                // TODO add auto adjustable padding?
+                st7789_lcd_put16(pio, sm, 0x0000);
 
                 for (int x = 0; x < TEXTMODE_COLS; x++) {
                     const uint16_t offset = (y / 8) * (TEXTMODE_COLS * 2) + x * 2;
                     const uint8_t c = text_buffer[offset];
-                    const uint8_t colorIndex = text_buffer[offset+1];
+                    const uint8_t colorIndex = text_buffer[offset + 1];
                     const uint8_t glyph_row = fnt6x8[c * 8 + y % 8];
 
-                    for (uint_fast8_t bit = 0; bit < 6; bit++) {
-                        const uint16_t color = textmode_palette[ (c && CHECK_BIT(glyph_row, bit)) ? colorIndex & 0x0F : (colorIndex >> 4) & 0x0F];
-                        st7789_lcd_put(pio, sm, color >> 8);
-                        st7789_lcd_put(pio, sm, color & 0xff);
+                    for (uint8_t bit = 0; bit < 6; bit++) {
+                        st7789_lcd_put16(pio, sm, textmode_palette[(c && CHECK_BIT(glyph_row, bit))
+                                                                       ? colorIndex & 0x0F
+                                                                       : colorIndex >> 4 & 0x0F]);
                     }
                 }
-                st7789_lcd_put(pio, sm, 0x00);
-                st7789_lcd_put(pio, sm, 0x00);
+                st7789_lcd_put16(pio, sm, 0x0000);
             }
             break;
+        case VGA_320x200x256:
         default: {
             const uint8_t* bitmap = graphics_buffer;
 
-            lcd_set_window(pio, sm, graphics_buffer_shift_x, graphics_buffer_shift_y, graphics_buffer_width,
-               graphics_buffer_height);
-            start_pixels(pio, sm);
+            lcd_set_window(graphics_buffer_shift_x, graphics_buffer_shift_y, graphics_buffer_width,
+                           graphics_buffer_height);
+            start_pixels();
 
             uint32_t i = graphics_buffer_width * graphics_buffer_height;
-            do {
-                const uint16_t color = palette[*bitmap++];
-                st7789_lcd_put(pio, sm, color >> 8);
-                st7789_lcd_put(pio, sm, color & 0xff);
+            while (i--) {
+                st7789_lcd_put16(pio, sm, palette[*bitmap++]);
             }
-            while (--i);
         }
     }
 
@@ -248,8 +243,8 @@ void __scratch_y("refresh_lcd") refresh_lcd() {
 }
 
 
-void graphics_set_palette(uint8_t i, uint32_t color) {
-    palette[i] = color;
+void graphics_set_palette(const uint8_t i, const uint32_t color) {
+    palette[i] = (uint16_t) color;
 }
 
 void logMsg(char* msg) {
