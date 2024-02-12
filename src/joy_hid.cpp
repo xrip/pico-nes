61e9363a63427439e4abf895cb37a190804dfeee
xrip <xrip@xrip.ru>
2024-02-12 13:42:44 +0300

    xinput working

--- src/joy_hid.cpp
+++ src/joy_hid.cpp
@@ -11,27 +11,28 @@ struct input_bits_t {
     bool up: true;
     bool down: true;
 };
+
 extern struct input_bits_t gamepad1_bits;
 
 //Since https://github.com/hathach/tinyusb/pull/2222, we can add in custom vendor drivers easily
-usbh_class_driver_t const* usbh_app_driver_get_cb(uint8_t* driver_count){
+usbh_class_driver_t const* usbh_app_driver_get_cb(uint8_t* driver_count) {
     *driver_count = 1;
     return &usbh_xinput_driver;
 }
 
-void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const *report, uint16_t len)
-{
+void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
     auto xid_itf = (xinputh_interface_t *)report;
-    const xinput_gamepad_t *p = &xid_itf->pad;
+    const xinput_gamepad_t* p = &xid_itf->pad;
 
-    gamepad1_bits.a= p->wButtons & XINPUT_GAMEPAD_A;
-    gamepad1_bits.b= p->wButtons & XINPUT_GAMEPAD_B;
+    gamepad1_bits.a = p->wButtons & XINPUT_GAMEPAD_A;
+    gamepad1_bits.b = p->wButtons & XINPUT_GAMEPAD_B;
 
 
     if (p->wButtons & XINPUT_GAMEPAD_GUIDE) {
         gamepad1_bits.start = true;
         gamepad1_bits.select = true;
-    } else {
+    }
+    else {
         gamepad1_bits.start = p->wButtons & XINPUT_GAMEPAD_START;
         gamepad1_bits.select = p->wButtons & XINPUT_GAMEPAD_BACK;
     }
@@ -43,8 +44,8 @@ void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t c
 
         gamepad1_bits.right = p->sThumbLX > 0 || p->sThumbRX > 0;
         gamepad1_bits.left = p->sThumbLX < 0 || p->sThumbRX < 0;
-
-    } else {
+    }
+    else {
         gamepad1_bits.down = dpad & XINPUT_GAMEPAD_DPAD_DOWN;
         gamepad1_bits.up = dpad & XINPUT_GAMEPAD_DPAD_UP;
         gamepad1_bits.left = dpad & XINPUT_GAMEPAD_DPAD_LEFT;
@@ -59,13 +60,11 @@ void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t c
     tuh_xinput_receive_report(dev_addr, instance);
 }
 
-void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance, const xinputh_interface_t *xinput_itf)
-{
+void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance, const xinputh_interface_t* xinput_itf) {
     TU_LOG1("XINPUT MOUNTED %02x %d\n", dev_addr, instance);
     // If this is a Xbox 360 Wireless controller we need to wait for a connection packet
     // on the in pipe before setting LEDs etc. So just start getting data until a controller is connected.
-    if (xinput_itf->type == XBOX360_WIRELESS && xinput_itf->connected == false)
-    {
+    if (xinput_itf->type == XBOX360_WIRELESS && xinput_itf->connected == false) {
         tuh_xinput_receive_report(dev_addr, instance);
         return;
     }
@@ -76,8 +75,6 @@ void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance, const xinputh_inter
     tuh_xinput_receive_report(dev_addr, instance);
 }
 
-void tuh_xinput_umount_cb(uint8_t dev_addr, uint8_t instance)
-{
-
+void tuh_xinput_umount_cb(uint8_t dev_addr, uint8_t instance) {
     TU_LOG1("XINPUT UNMOUNTED %02x %d\n", dev_addr, instance);
 }
