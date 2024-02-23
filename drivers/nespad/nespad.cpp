248e0b0827129d59233c7c6a98477f41de0be47e
xrip <xrip@xrip.ru>
2024-02-23 09:50:26 +0300

    (s)nes pad refactoring by dee

--- drivers/nespad/nespad.cpp
+++ drivers/nespad/nespad.cpp
@@ -7,7 +7,7 @@ static const uint16_t nespad_program_instructions[] = {
     //     .wrap_target
     0x80a0, //  0: pull   block  
     0xea01, //  1: set    pins, 1         side 0 [10]
-    0xe027, //  2: set    x, 7            side 0
+    0xe02f, //  2: set    x, 15           side 0
     0xe000, //  3: set    pins, 0         side 0
     0x4402, //  4: in     pins, 2         side 0 [4]      <--- 2
     0xf500, //  5: set    pins, 0         side 1 [5]
@@ -30,11 +30,10 @@ static inline pio_sm_config nespad_program_get_default_config(uint offset) {
 
 static PIO pio = pio1;
 static uint8_t sm = -1;
-uint8_t nespad_state  = 0;  // Joystick 1
-uint8_t nespad_state2 = 0;  // Joystick 2
+uint32_t nespad_state  = 0;  // Joystick 1
+uint32_t nespad_state2 = 0;  // Joystick 2
 
-bool nespad_begin(uint32_t cpu_khz, uint8_t clkPin, uint8_t dataPin,
-                  uint8_t latPin) {
+bool nespad_begin(uint32_t cpu_khz, uint8_t clkPin, uint8_t dataPin,uint8_t latPin) {
   if (pio_can_add_program(pio, &nespad_program) &&
       ((sm = pio_claim_unused_sm(pio, true)) >= 0)) {
     uint offset = pio_add_program(pio, &nespad_program);
@@ -55,7 +54,7 @@ bool nespad_begin(uint32_t cpu_khz, uint8_t clkPin, uint8_t dataPin,
                                  (1 << clkPin) | (1 << latPin) | 
                                  (1 << dataPin) | (1 << (dataPin+1))
                                 ); // All pins
-    sm_config_set_in_shift(&c, true, true, 16); // R shift, autopush @ 8 bits (@ 16 bits for 2 Joystick)
+    sm_config_set_in_shift(&c, true, true, 32); // R shift, autopush @ 8 bits (@ 16 bits for 2 Joystick)
 
     sm_config_set_clkdiv_int_frac(&c, cpu_khz / 1000, 0); // 1 MHz clock
 
@@ -88,40 +87,10 @@ void nespad_read()
 
   // Right-shift was used in sm config so bit order matches NES controller
   // bits used elsewhere in picones, but does require shifting down...
-  uint16_t temp16=((pio->rxf[sm])>>16)^ 0xFFFF;
+  uint32_t temp=pio->rxf[sm]^ 0xFFFFFFFF;
   pio->txf[sm]=0;
-  uint16_t temp1, temp2;
-  // temp1  = temp16 & 0x5555;             // 08070605.04030201
-  // temp2  = temp16 & 0xAAAA;             // 80706050.40302010
-  // nespad_state=temp1|(temp1>>7); //84736251
-  //nespad_state2=(temp2|(temp2<<7))>>8;//84736251
-  // return;
-
-
-// 1 -------------------------------------------------------  
-  temp1  = temp16 & 0x5555;             // 08070605.04030201
-  temp2  = temp16 & 0xAAAA;             // 80706050.40302010
-  temp16 = temp16 & 0xAA55;             // 80706050.04030201
-  temp1  = temp1 >> 7;                  // 00000000.80706050
-  temp2  = temp2 << 7;                  // 04030201.00000000
-  temp16 = temp16 | temp1 | temp2;      // 84736251.84736251
-// 2 -------------------------------------------------------
-  temp1  = temp16 & 0x5050;             // 04030000.04030000
-  temp2  = temp16 & 0x0A0A;             // 00006050.00006050
-  temp16 = temp16 & 0xA5A5;             // 80700201.80700201
-  temp1  = temp1 >> 3;                  // 00004030.00004030
-  temp2  = temp2 << 3;                  // 06050000.06050000
-  temp16 = temp16 | temp1 | temp2;      // 86754231.86754231
-// 3 -------------------------------------------------------
-  temp1  = temp16 & 0x4444;             // 06000200.06000200
-  temp2  = temp16 & 0x2222;             // 00700030.00700030
-  temp16 = temp16 & 0x9999;             // 80054001.80054001
-  temp1  = temp1 >> 1;                  // 00600020.00600020
-  temp2  = temp2 << 1;                  // 07000300.07000300
-  temp16 = temp16 | temp1 | temp2;      // 87654321.87654321
-//----------------------------------------------------------
-  nespad_state  = temp16;               // 00000000.87654321 Joy1
-  nespad_state2 = temp16 >> 8;          // 00000000.87654321 Joy2
+  nespad_state  = temp & 0x555555;         //  Joy1
+  nespad_state2 = temp >> 1 & 0x555555;  //  Joy2
 }
 
 
--- drivers/nespad/nespad.h
+++ drivers/nespad/nespad.h
@@ -1,16 +1,21 @@
 #pragma once
 
-#define DPAD_LEFT 0x40
-#define DPAD_RIGHT 0x80
-#define DPAD_DOWN 0x20
-#define DPAD_UP 0x10
-#define DPAD_START 0x08
-#define DPAD_SELECT 0x04
-#define DPAD_B 0x02
-#define DPAD_A 0x01
+#define DPAD_LEFT   0x001000
+#define DPAD_RIGHT  0x004000
+#define DPAD_DOWN   0x000400
+#define DPAD_UP     0x000100
+#define DPAD_START  0x000040
+#define DPAD_SELECT 0x000010
+#define DPAD_B      0x000004   //Y on SNES
+#define DPAD_A      0x000001   //B on SNES
 
-extern uint8_t nespad_state;    // NES Joystick1
-extern uint8_t nespad_state2;    // NES Joystick1
+#define DPAD_Y       0x010000   //A on SNES
+#define DPAD_X       0x040000
+#define DPAD_LT      0x100000
+#define DPAD_RT      0x400000
+
+extern uint32_t nespad_state;  // (S)NES Joystick1
+extern uint32_t nespad_state2; // (S)NES Joystick2
 
 extern bool nespad_begin(uint32_t cpu_khz, uint8_t clkPin, uint8_t dataPin,
                          uint8_t latPin);
